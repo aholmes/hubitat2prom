47be1e7bbb782465c2a7dba11510bf54de6152a8
Aaron Holmes <aaron@aaronholmes.net>
2022-03-03 20:49:05 -0800

    Add Dockerfile and disable HSTS.

--- hubitat2prom/Startup.cs
+++ hubitat2prom/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using hubitat2prom.JsonConverters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace hubitat2prom;
@@ -28,7 +20,6 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
         services
             .AddControllers()
             .AddJsonOptions(options =>
@@ -59,8 +50,6 @@ public class Startup
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "hubitat2prom v1"));
         }
-
-        app.UseHttpsRedirection();
 
         app.UseRouting();
 
